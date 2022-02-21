5bf93b018f3e844b94e81a5e1149a695f96e5dc5
l0w0l-Y <79462005+l0w0l-Y@users.noreply.github.com>
2022-02-21 21:57:17 +0300

    Refactoring.

--- Task1/task_1.py
+++ Task1/task_1.py
@@ -6,13 +6,13 @@ import os
 # Save all received images to the original folder.
 # =============================================================================
 
-# Entering the size of the cropped images.
+# Enters the size of the cropped images.
 stepXY = int(input("Enter the size of the cropped image: "))
 
 # Path to save cropped images.
 save_path = "cropped_images"
 
-# Image reading.
+# Gets an image.
 img = cv2.imread("photo_test.jpg")
 
 # Image width.
@@ -22,17 +22,17 @@ imgXMax = img.shape[0]
 imgYMax = img.shape[1]
 
 
-# Cropping the image. Showing it and writing it to a specific directory.
+# Crops the image. Showing it and writing it to a specific directory.
 def readAndShowCroppedImage(i, j, step):
-    # Cropping the image from i pixel to i + step pixel x-axis and j to j+step y-axis.
+    # Crops the image from i pixel to i + step pixel x-axis and j to j+step y-axis.
     imgCropped = img[i:i + step, j: j + step]
-    # Showing the cropped image.
+    # Shows the cropped image.
     cv2.imshow(f"cropped_image_{str(i)}_{str(j)}", imgCropped)
-    # Saving the cropped image in current directory.
+    # Saves the cropped image in current directory.
     cv2.imwrite(f"{save_path}/{stepXY}/cropped_image_{str(i)}_{str(j)}.jpg", imgCropped)
 
 
-# If the step size is larger than the image size, cropping is not possible.
+# If the step size is larger than the image size, crops is not possible.
 if (stepXY > imgXMax) or (stepXY > imgYMax):
     print("You can't crop the image. Step larger than the image size.")
 else:
@@ -40,23 +40,24 @@ else:
     path = os.path.join(save_path, str(stepXY))
     # If the directory with path doesn't exist, it creates a new one. Otherwise, it deletes all files from it.
     if not os.path.isdir(path):
-        # Create new directory with path.
+        # Creates new directory with path.
         os.makedirs(path)
     else:
-        # Getting all files from directory with path.
+        # Gets all files from directory with path.
         allfiles = os.listdir(path)
         for file in allfiles:
             filePath = os.path.join(path, file)
-            # Delete file.
+            # Deletes file.
             os.remove(filePath)
 
     for x in range(0, imgXMax, stepXY):
         for y in range(0, imgYMax, stepXY):
-            # If a piece of the leftover image is smaller than our step, we do not crop it,
-            # because we only need images of the height and width of this step.
+            # If a piece of the leftover image is smaller than our step, we don't crop it,
+            # because only an image of this step is required.
             if (imgYMax - y) < stepXY or (imgXMax - x) < stepXY:
                 break
             else:
                 readAndShowCroppedImage(x, y, stepXY)
+
 # Doesn't close the window when the code ends.
 cv2.waitKey(0)
--- Task2/task_2.py
+++ Task2/task_2.py
@@ -12,25 +12,25 @@ from numpy import float32
 # The name of the editor window.
 window_name = "Editor window"
 
-# Entering the wrapped image width.
+# Enters the wrapped image width.
 width = int(input("Enter the wrapped image width: "))
 
-# Entering the wrapped image height.
+# Enters the wrapped image height.
 height = int(input("Enter the wrapped image height: "))
 
-# Containing the editing image name.
+# Contains the editing image name.
 img_name = "cards_image.jpg"
 
-# Image reading. It doesn't change.
+# Gets an Image. It doesn't change.
 init_img = cv2.imread(img_name)
 
-# Image that changes in the program.
+# Gets an Image. Image that changes in the program.
 img = cv2.imread(img_name)
 
-# Saving image to cache
+# Saves image to cache.
 cache = img
 
-# Count of selected points. Can wrapping the image if the number of points is 4.
+# Count of selected points. Can wrap the image if the number of points is 4.
 pointCount = 0
 
 # Whether the images returned to the cache.
@@ -43,9 +43,9 @@ wrappedImagePoints = np.zeros((4, 2), float32)
 save_path = "wrapped_images"
 
 
-# Drawing circle on the image.
+# Draws circle on the image.
 def draw_circle(event, x, y, flags, param):
-    # Using global values.
+    # Uses global values.
     global img
     global cache
     global pointCount
@@ -53,57 +53,57 @@ def draw_circle(event, x, y, flags, param):
     global isBack
     # Event is equal to double-clicking the left mouse button.
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        # Saving condition image on the cache.
+        # Saves condition image on the cache.
         cache = copy.deepcopy(img)
         # Added new cache. Haven't called it yet.
         isBack = False
-        # Adding a point to the array.
+        # Adds a point to the array.
         wrappedImagePoints[pointCount] = [float32(x), float32(y)]
-        # Increasing a point count.
+        # Increases a point count.
         pointCount += 1
-        # Drawing circle on the image.
+        # Draws circle on the image.
         cv2.circle(img, (x, y), 5, (255, 0, 230), -1)
     else:
         # Event is equal to clicking the right mouse button.
         if event == cv2.EVENT_RBUTTONDOWN:
             if pointCount > 0:
                 if not isBack:
-                    # Returning condition image from the cache.
+                    # Returns condition image from the cache.
                     img = copy.deepcopy(cache)
-                    # Have called cache.
+                    # The cache has been returned.
                     isBack = True
-                    # Reducing a point count.
+                    # Reduces a point count.
                     pointCount -= 1
 
 
-# Showing the image.
+# Shows the image.
 cv2.imshow(window_name, img)
 
-# Setting mouse callback function to the window.
+# Sets mouse callback function to the window.
 cv2.setMouseCallback(window_name, draw_circle)
 while True:
-    # Constantly refreshing the image.
+    # Constantly refreshes the image.
     cv2.imshow(window_name, img)
     # Containing index of pressed key.
     key = cv2.waitKey(20) & 0xFF
-    # Entering ESC button.
+    # Enters ESC button.
     if key == 27:
         break
-    # Entering Space button.
+    # Enters Space button.
     if key == ord(' '):
         if pointCount == 4:
             sideMatrix = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
-            # Getting perspective transform with 2 float arrays.
+            # Gets perspective transform with 2 float arrays.
             matrix = cv2.getPerspectiveTransform(wrappedImagePoints, sideMatrix)
             # The wrapped image.
             wrappedImg = cv2.warpPerspective(init_img, matrix, (width, height))
-            # Destroying the original image window
+            # Destroys the original image window
             cv2.destroyWindow(window_name)
-            # Saving the wrapped image.
+            # Saves the wrapped image.
             cv2.imwrite(f"{save_path}/wrapped_image_{img_name}", wrappedImg)
-            # Showing the wrapped image.
+            # Shows the wrapped image.
             cv2.imshow("Wrapped_image", wrappedImg)
             # Doesn't close the window when the code ends.
             cv2.waitKey(0)
-            # Breaking out of the eternal loop.
+            # Breaks out of the eternal loop.
             break
